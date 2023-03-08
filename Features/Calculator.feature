267c96127faa65274995729fd7c0ee50e52e44f1
nuno.fernandes <nuno.fernandes@stonex.com>
2023-03-08 09:59:03 +0100

    Add project files.

---
+++ Features/Calculator.feature
@@ -0,0 +1,10 @@
+﻿Feature: Calculator
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for adding **two** numbers
+
+Link to a feature: [Calculator](SpecFlowProject2/Features/Calculator.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@retry(5)
+Scenario: Add two numbers
+	Then the first number is 50
---
+++ StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,20 @@
+namespace SpecFlowProject2.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        [Then("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            Assert.Equal(1, 2);
+        }
+    }
+}
